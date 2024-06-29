0b759e9b7819649eef7ccb1f81373c63321b70d5
silvinomar <silvinomartins991@gmail.com>
2024-06-29 11:15:43 +0100

    improved progress bar feedback

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 
 class ProgressBar extends Component {
     render() {
-        const { progress, loaded, total } = this.props;
+        const { progress, loaded, total, teamName } = this.props;
 
         return (
             <div className="px-4 py-3 progress-bar-container mb-5">
+                <p className="lead">Loading <b>{teamName}</b> stats.</p>
                 <p className="lead">Getting details from {loaded} of {total} players...</p>
                 <div
                     className="progress-bar"
                     style={{ width: `${progress}%` }}
                 >
-                    
+
                 </div>
             </div>
         );
